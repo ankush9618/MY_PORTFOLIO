1b6e5094f5498e26c5449cced9ebf210fa84702f
Ankush Kumar <ankushkumar9618@gmail.com>
2025-07-23 01:47:18 +0530

    Added About Section

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,16 +1,24 @@
 import React from 'react'
+import SectionName from '../SectionName'
 
 function About() {
   return (
-    <div className='lg:w-4/5 mx-auto w-9/10 text-center'>
-        <div className="about-heading">
+    <div className='lg:w-7/10 mx-auto w-9/10 text-center mb-10 md:mt-30 mt-20'>
+        <div className="about-heading md:mb-20 mb-10">
+            <SectionName name="About Me"/>
+        </div>
+        <div className="about-me flex gap-8 text-start w-full">
+            <div className="about-image min-w-80 bg-blue-400 h-90 rounded-xl hidden md:block">
 
-        </div>
-        <div className="about-me">
-            I'm a Siebel developer with over 1.5 years of experience working on Siebel (Open UI) at Accenture. Currently, I am passionately expanding my expertise in modern web development—crafting responsive and engaging user interfaces with technologies like React, Next.js, Tailwind CSS, Bootstrap, and traditional HTML/CSS. I enjoy building intuitive digital experiences, leveraging both cutting-edge frameworks and classic web foundations, and continually embracing new challenges to grow as a developer.
-        </div>
-        <div className="about-me">
-            I'm a Siebel developer with over 1.5 years of experience working on Siebel (Open UI) at Accenture. Currently, I am passionately expanding my expertise in modern web development—crafting responsive and engaging user interfaces with technologies like React, Next.js, Tailwind CSS, Bootstrap, and traditional HTML/CSS. I enjoy building intuitive digital experiences, leveraging both cutting-edge frameworks and classic web foundations, and continually embracing new challenges to grow as a developer.
+            </div>
+            <div className="about-info  mx-auto text-justify text-sm md:text-base">
+                <p className='mb-3'>
+                    I'm a Siebel developer with over 1.5+ years of experience working on Siebel (Open UI) at Accenture. Currently, I am passionately expanding my expertise in modern web development—crafting responsive and engaging user interfaces with technologies like React, Next.js, Tailwind CSS, Bootstrap, and traditional HTML/CSS.
+                </p>
+                <p>
+                    Skilled in JavaScript, React.js, Node.js, HTML, CSS, Bootstrap, Tailwind CSS, and Siebel Open UI, with a strong grasp of frontend fundamentals and modern development practices. Consistently strives to deliver intuitive, user-friendly interfaces that align with both user needs and business goals.
+                </p>
+            </div>
         </div>
     </div>
   )
--- src/components/header/Navbar.jsx
+++ src/components/header/Navbar.jsx
@@ -3,7 +3,8 @@ import { CgMenuRightAlt } from "react-icons/cg";
 import { RxCross2 } from "react-icons/rx";
 import git from "../../assets/git.png"
 import linkedIn from "../../assets/linkedIn.png"
-import instagram from "../../assets/instagram.png"
+import instagram from "../../assets/instagram.png";
+import { IoIosArrowDown } from "react-icons/io";
 
 function Navbar() {
 
@@ -14,7 +15,7 @@ function Navbar() {
     console.log(menu)
     return (
         <>
-            <div className="navbar w-full sticky top-0 bg-[#1c1c1c]">
+            <div className="navbar w-full sticky top-0 bg-[#1c1c1c] z-100">
                 <div className="navbar-big lg:flex justify-between w-9/10 h-25 items-center hidden mx-auto">
                     <div className="navbar-left text-3xl font-semibold relative cursor-pointer">
                         <p className='z-1 relative bg-transparent name text-red'>Ankush Kumar Singh</p>
@@ -23,10 +24,21 @@ function Navbar() {
                     <div className="navbar-middle text-xl">
                         <ul className='flex gap-8'>
                             <li>Home</li>
-                            <li>About Me</li>
+                            <li>About me</li>
+                            <li>Experience</li>
                             <li>Skills</li>
                             <li>Projects</li>
-                            <li>Contact</li>
+                            <li className='group cursor-pointer relative'>
+                                <p className='flex items-center gap-2'>Other<IoIosArrowDown/></p>
+                                <div className='hidden group-hover:block absolute top-7 w-50 pt-5'>
+                                    <ul className='flex flex-col gap-2 bg-[#3c3737] border-t-2 border-pink-400 px-5'>
+                                        <li>Education</li>
+                                        <li>Certifications</li>
+                                        <li></li>
+                                    </ul>
+                                </div>
+                            </li>
+                            
                         </ul>
                     </div>
                     <div className="navbar-right px-4  py-3 rounded-3xl hover:scale-104 cursor-pointer transition-scale duration-500">
@@ -41,7 +53,7 @@ function Navbar() {
                         </div>
                         {menu ? <RxCross2 className='text-3xl' onClick={menuclicked}/> :<CgMenuRightAlt className='text-3xl' onClick={menuclicked} />}
                     </div>
-                    <div className={`fixed top-20 z-50 w-8/10 h-[calc(100vh-80px)] bg-white transition-all duration-800 ease-in-out ${menu ? 'right-0' : '-right-full'}`}>
+                    <div className={`fixed top-21 z-50 w-8/10 h-[calc(100vh-80px)] bg-white transition-all duration-800 ease-in-out ${menu ? 'right-0' : '-right-full'}`}>
                         <ul className='bg-[#3c3737] from-[#DA7C25] to-[#B923E1] flex flex-col gap-4 py-10 px-8 text-start h-full'>
                             <li>Home</li>
                             <li>About Me</li>
