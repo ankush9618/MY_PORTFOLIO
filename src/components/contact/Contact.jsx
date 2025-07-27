0cc442cf5832a3ecd7448d2fd1d51d4c1ec73dbd
Ankush Kumar <ankushkumar9618@gmail.com>
2025-07-27 05:33:56 +0530

    Fixing a bug due to fixed width in contact section it was scrolling horizontally

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -23,14 +23,14 @@ function Contact() {
                             <div className="email flex items-center gap-2"><IoLocationSharp className='text-xl'/> India</div>
                         </div>
                     </div>
-                    <div className='text-start w-100 '>
+                    <div className='text-start w-full'>
                         <form action="">
                             <label htmlFor="name" className='text-start'>Your Name</label>
                             <input type="text" placeholder='Enter your name' id="name" className='md:w-full border-2 px-2 py-1 block mt-2 mb-5 rounded-md border-gray-400'/>
                             <label htmlFor="email" className='text-start'>Your Email</label>
                             <input type="email" placeholder='Enter your email' id="email" className='md:w-full border-2 px-2 py-1 block mt-2 mb-5 rounded-md border-gray-400'/>
                             <label htmlFor="message" className='text-start'>Your Email</label>
-                            <textarea type="text" placeholder='Enter your message here..' id="message" className='md:w-full border-2 px-2 py-1 block mt-2 mb-5 rounded-md border-gray-400' rows={5} cols={30}/>
+                            <textarea type="text" placeholder='Enter your message here..' id="message" className='md:w-full border-2 px-2 py-1 block mt-2 mb-5 rounded-md border-gray-400' rows={5} cols={25}/>
                             <input type="submit" value="Submit" className='bgGradient px-5 py-3 text-xl rounded-3xl cursor-pointer hover:scale-104 duration-700 text-white'/>
                         </form>
                     </div>
