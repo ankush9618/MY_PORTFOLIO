72d9b90103bb28d6beae2bc5a77b89c95daf9555
Ankush Kumar <ankushkumar9618@gmail.com>
2025-07-29 23:57:22 +0530

    Adding few thing to make it responsive for md

--- src/components/utils/SkillsCard.jsx
+++ src/components/utils/SkillsCard.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 function SkillsCard({skills}) {
     return (
         <>
-            <div className='xl:w-90 lg:w-90 w-70  h-auto border-2 border-white hover:border-[#ff00ff] rounded-xl hover:scale-110 duration-500 skillcard flex items-center justify-center cursor-pointer flex-col py-4 px-2 md:gap-4 gap-2 shadow-md shadow-gray-700 outline-0'>
+            <div className='xl:w-90 md:w-80 w-70  h-auto border-2 border-white hover:border-[#ff00ff] rounded-xl hover:scale-110 duration-500 skillcard flex items-center justify-center cursor-pointer flex-col py-4 px-2 md:gap-4 gap-2 shadow-md shadow-gray-700 outline-0'>
                 <div className={`textClip md:text-2xl text-xl font-semibold `}> {skills.name}</div>
                 <div className='text-start md:text-xl mx-auto pl-2 text-gray-300 text-md'>{skills.description}</div>
                 <div className='flex flex-wrap gap-2 pl-2'>{
