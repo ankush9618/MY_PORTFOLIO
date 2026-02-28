57bebd8e92ba4a3ea69c2c86bcc4a033f4ca5bb4
Ankush Kumar <ankushkumar9618@gmail.com>
2026-02-28 23:57:07 +0530

    Fixing Dynamic class namming issue due to which tailwing is not creating CSS during the build

--- src/components/utils/CerificateCard.jsx
+++ src/components/utils/CerificateCard.jsx
@@ -1,26 +1,26 @@
 import React, { useState } from 'react';
 import { IoCloseSharp } from "react-icons/io5";
 
-function CerificateCard({certificate}) {
-    const [active,setActive] = useState(false);
+function CerificateCard({ certificate }) {
+    const [active, setActive] = useState(false);
 
-    const handleClick = ()=>{
+    const handleClick = () => {
         setActive(true);
         document.body.classList.add('overflow-hidden');
     }
 
-    const handleClose = ()=>{
+    const handleClose = () => {
         setActive(false);
         document.body.classList.remove('overflow-hidden');
     }
     return (
         <>
-        <div className={`row-end-[span_${certificate.height}] rounded-xl overflow-hidden hover:scale-105 duration-500 cursor-pointer relative bg-white/10`}>
-            <img onClick={handleClick} src={certificate.img} alt={certificate.name} className='w-full rounded-xl p-0.5 object-cover h-full opacity-80' loading='lazy' />
-        </div>
-        {
-            active && <div className='fixed top-0 left-0 p-5 flex justify-center items-center z-100 h-screen w-full bg-white/10 backdrop-blur-xs'><div className={`${certificate.height>7 ? "w-100": "w-200"} relative flex flex-col md:flex-col-reverse gap-4`}><img src={certificate.img} alt="" className='w-full rounded-xl cursor-pointer'/> <div className='bg-black/10 hover:scale-110 duration-300 md:bg-white/20 size-10 md:size-12 mx-auto rounded-full flex justify-center items-center text-7xl text-white cursor-pointer' onClick={handleClose}><IoCloseSharp/></div></div></div>
-        }
+            <div className={`row-end-span_${certificate.height} rounded-xl overflow-hidden hover:scale-105 duration-500 cursor-pointer relative bg-white/10`}>
+                <img onClick={handleClick} src={certificate.img} alt={certificate.name} className='w-full rounded-xl p-0.5 object-cover h-full opacity-80' loading='lazy' />
+            </div>
+            {
+                active && <div className='fixed top-0 left-0 p-5 flex justify-center items-center z-100 h-screen w-full bg-white/10 backdrop-blur-xs'><div className={`${certificate.height > 7 ? "w-100" : "w-200"} relative flex flex-col md:flex-col-reverse gap-4`}><img src={certificate.img} alt="" className='w-full rounded-xl cursor-pointer' /> <div className='bg-black/10 hover:scale-110 duration-300 md:bg-white/20 size-10 md:size-12 mx-auto rounded-full flex justify-center items-center text-7xl text-white cursor-pointer' onClick={handleClose}><IoCloseSharp /></div></div></div>
+            }
         </>
     )
 }
